88be02d3d15a0e480cf9de73db281e59f9219b0f
Jamie Cartin <myaakova@gmail.com>
2022-11-11 22:30:21 -0500

    Animate sprites

--- src/main/Game.java
+++ src/main/Game.java
@@ -39,7 +39,6 @@ public class Game implements Runnable {
             }
 
 
-
             if (System.currentTimeMillis() - lastCheck >= 1000) {
                 lastCheck = System.currentTimeMillis();
                 System.out.println("FPS " + frames);
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -14,7 +14,9 @@ public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
     private float xDelta = 100, yDelta = 100;
-    private BufferedImage img, subImg;
+    private BufferedImage img;
+    private BufferedImage[] idleAni;
+    private int aniTick, aniIndex, aniSpeed = 15;
 
 
 
@@ -22,11 +24,20 @@ public class GamePanel extends JPanel {
         mouseInputs = new MouseInputs(this);
 
         importImg();
+        loadAnimations();
 
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
+
+    private void loadAnimations() {
+        idleAni = new BufferedImage[5];
+
+        for (int i = 0; i < idleAni.length; i++) {
+            idleAni[i] = img.getSubimage(i * 64, 0, 64, 40);
+        }
     }
 
     private void importImg() {
@@ -36,14 +47,18 @@ public class GamePanel extends JPanel {
             img = ImageIO.read(is);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private void setPanelSize() {
         Dimension size = new Dimension(1280, 800);
-        setMinimumSize(size);
         setPreferredSize(size);
-        setMaximumSize(size);
     }
 
     public void changeXDelta(int value) {
@@ -59,11 +74,23 @@ public class GamePanel extends JPanel {
         this.yDelta = y;
     }
 
+    private void updateAnimationTick() {
+        aniTick++;
+        if (aniTick >= aniSpeed) {
+            aniTick = 0;
+            aniIndex++;
+            if (aniIndex >= idleAni.length) {
+                aniIndex = 0;
+            }
+        }
+    }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        subImg = img.getSubimage(1*64, 8*40 , 64, 40);
-        g.drawImage(subImg,(int)xDelta, (int)yDelta, 128, 80 ,null);
+        updateAnimationTick();
+
+        g.drawImage(idleAni[aniIndex],(int)xDelta, (int)yDelta, 128, 80 ,null);
     }
 
 
