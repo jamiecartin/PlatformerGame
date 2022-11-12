94b254963d006b4ffc2fbba44b831b6e007a002c
Jamie Cartin <myaakova@gmail.com>
2022-11-12 16:07:22 -0500

    attack animation

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -14,7 +14,7 @@ public class Player extends Entity {
     private BufferedImage[][] animations;
     private int aniTick, aniIndex, aniSpeed = 15;
     private int playerAction = IDLE;
-    private boolean moving = false;
+    private boolean moving = false, attacking = false;
     private boolean left, up, right, down;
     private float playerSpeed = 2.0f;
 
@@ -41,15 +41,31 @@ public class Player extends Entity {
             aniIndex++;
             if (aniIndex >= GetSpriteAmount(playerAction)) {
                 aniIndex = 0;
+                attacking = false;
             }
         }
     }
     private void setAnimation() {
+        int startAni = playerAction;
+
         if (moving) {
             playerAction = RUNNING;
         } else {
             playerAction = IDLE;
         }
+
+        if(attacking) {
+            playerAction = ATTACK_1;
+        }
+
+        if (startAni != playerAction) {
+            resetAniTick();
+        }
+    }
+
+    private void resetAniTick() {
+        aniTick = 0;
+        aniIndex = 0;
     }
 
     private void updatePos() {
@@ -99,6 +115,10 @@ public class Player extends Entity {
         right = false;
         up = false;
         down = false;
+    }
+
+    public void setAttacking(boolean attacking) {
+        this.attacking = attacking;
     }
 
     public boolean isLeft() {
--- src/inputs/MouseInputs.java
+++ src/inputs/MouseInputs.java
@@ -15,6 +15,9 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            gamePanel.getGame().getPlayer().setAttacking(true);
+        }
 
     }
 
